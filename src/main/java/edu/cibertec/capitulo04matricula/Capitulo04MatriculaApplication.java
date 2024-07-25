ea951c5f41f6de98c9006ec91873b767a94a57fd
Julio Ucharima <julio_ucharima@usmp.pe>
2024-07-24 20:18:02 -0500

    init commit

---
+++ src/main/java/edu/cibertec/capitulo04matricula/Capitulo04MatriculaApplication.java
@@ -0,0 +1,13 @@
+package edu.cibertec.capitulo04matricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capitulo04MatriculaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Capitulo04MatriculaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/cibertec/capitulo04matricula/Capitulo04MatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cibertec.capitulo04matricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capitulo04MatriculaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
