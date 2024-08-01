fcefe5953362f5d081a6e60a49da52843302328c
Julio Ucharima <julio_ucharima@usmp.pe>
2024-08-01 11:49:42 -0500

    add CRUD methods to CursoController

--- src/main/java/edu/cibertec/capitulo04matricula/controller/CursoController.java
+++ src/main/java/edu/cibertec/capitulo04matricula/controller/CursoController.java
@@ -2,32 +2,60 @@ package edu.cibertec.capitulo04matricula.controller;
 
 import edu.cibertec.capitulo04matricula.dao.entity.CursoEntity;
 import edu.cibertec.capitulo04matricula.service.CursoService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Slf4j
 public class CursoController {
+
+    Logger logger = LoggerFactory.getLogger(CursoController.class);
 
     @Autowired
     private CursoService cursoService;
 
     @RequestMapping("cursoListar")
     public ModelAndView cursoMostrar() {
-        return new ModelAndView("curso", "lista", cursoService.listarTodos());
+        logger.debug("Listando los cursos");
+        ModelAndView mv = new ModelAndView("curso", "lista", cursoService.listarTodos());
+        mv.addObject("cursoBean", new CursoEntity());
+        return mv;
     }
 
     @RequestMapping("cursoGrabar")
-    public ModelAndView cursoGrabar(CursoEntity curso) {
-        cursoService.insertar(curso);
-        return new ModelAndView("redirect:cursoListar");
+    public ModelAndView cursoGrabar(@Valid @ModelAttribute("cursoBean") CursoEntity ce,
+                                    BindingResult valida) {
+        logger.info("Antes de insertar un curso");
+        ModelAndView mv = new ModelAndView("curso");
+        if (valida.hasErrors()) {
+            logger.warn("Falló el insertar curso");
+            mv.addObject("lista", cursoService.listarTodos());
+            mv.addObject("cursoBean", ce);
+        }
+        else
+        {
+            cursoService.insertar(ce);
+            mv.addObject("lista", cursoService.listarTodos());
+            mv.addObject("cursoBean", new CursoEntity());
+            logger.warn("Grabó el curso! " + ce.getIdCurso());
+        }
+        return mv;
     }
 
     @RequestMapping("cursoEliminar")
     public ModelAndView cursoEliminar(@RequestParam("codigo") int codigo) {
+        logger.info("Antes de eliminar un curso");
         cursoService.eliminar(codigo);
+        logger.warn("Después de eliminar un curso "+codigo);
         return new ModelAndView("redirect:cursoListar");
     }
 }
