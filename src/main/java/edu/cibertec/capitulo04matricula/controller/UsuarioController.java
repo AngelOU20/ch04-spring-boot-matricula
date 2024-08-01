8420cd330481fb29f0fba2b586f8a7619195ffe4
Julio Ucharima <julio_ucharima@usmp.pe>
2024-08-01 11:51:25 -0500

    add logs using SLf4j

--- src/main/java/edu/cibertec/capitulo04matricula/controller/UsuarioController.java
+++ src/main/java/edu/cibertec/capitulo04matricula/controller/UsuarioController.java
@@ -3,18 +3,21 @@ package edu.cibertec.capitulo04matricula.controller;
 import ch.qos.logback.core.model.Model;
 import edu.cibertec.capitulo04matricula.dao.entity.UsuarioEntity;
 import edu.cibertec.capitulo04matricula.service.UsuarioService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Slf4j
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
     @RequestMapping("/")
     public String loginMostrar(Model model) {
+        log.debug("Ingresando al login");
         return "login";
     }
 
@@ -23,10 +26,12 @@ public class UsuarioController {
         ModelAndView mv = null;
         UsuarioEntity ue = usuarioService.validarLogin(usuarioValida);
         if (ue == null) {
+            log.info("Error en el login");
             mv = new ModelAndView("login",
                     "msgError",
                     "Usuario y clave no existen. Vuelva a intentar!");
         } else {
+            log.warn("Usuario ingresando a la aplicación "+ue.getUsuario());
             mv = new ModelAndView("principal", "usuario", ue);
         }
         return mv;
